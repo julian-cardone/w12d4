5dd2308555498413bd1eaa78fcc8c50f3b639866
Julian Cardone <juliancardone1@gmail.com>
2022-12-01 14:18:19 -0500

    bench store setup:

--- bench-bnb/frontend/src/index.js
+++ bench-bnb/frontend/src/index.js
@@ -1,51 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { BrowserRouter } from 'react-router-dom';
-// import './index.css';
-// import App from './App';
-// import configureStore from './store';
-// import csrfFetch, { restoreCSRF } from './store/csrf';
-// import * as sessionActions from './store/session';
-
-// const store = configureStore();
-
-// if (process.env.NODE_ENV !== 'production') {
-//   window.store = store;
-//   window.csrfFetch = csrfFetch;
-//   window.sessionActions = sessionActions;
-// }
-
-// function Root() {
-//   return (
-//     <Provider store={store}>
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>
-//     </Provider>
-//   );
-// }
-
-// const renderApplication = () => {
-//   ReactDOM.render(
-//     <React.StrictMode>
-//       <Root />
-//     </React.StrictMode>,
-//     document.getElementById('root')
-//   );
-// }
-
-// if (
-//   sessionStorage.getItem("currentUser") === null ||
-//   sessionStorage.getItem("X-CSRF-Token") === null
-// ) {
-//   store.dispatch(sessionActions.restoreSession()).then(renderApplication);
-// } else {
-//   renderApplication();
-// }
-
-// frontend/src/index.js
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
@@ -56,6 +8,7 @@ import App from "./App";
 import configureStore from "./store";
 import csrfFetch from "./store/csrf";
 import * as sessionActions from "./store/session";
+import * as benchActions from './store/benches';
 
 const store = configureStore();
 
@@ -63,6 +16,7 @@ if (process.env.NODE_ENV !== "production") {
   window.store = store;
   window.csrfFetch = csrfFetch;
   window.sessionActions = sessionActions;
+  window.benchActions = benchActions;
 }
 
 function Root() {
---
+++ bench-bnb/frontend/src/store/benches.js
@@ -0,0 +1,58 @@
+import csrfFetch from "./csrf"
+
+export const SET_BENCHES = "benches/SET_BENCHES"
+export const ADD_BENCH = "benches/ADD_BENCH"
+
+export const setBenches = (benches) =>{
+  return {
+    type: SET_BENCHES,
+    benches
+  }
+}
+
+export const addBench = (bench) =>{
+  return {
+    type: ADD_BENCH,
+    bench
+  }
+}
+
+export const fetchBenches = () => async (dispatch) =>{
+  const res = await csrfFetch("/api/benches")
+
+  const data = await res.json();
+
+  dispatch(setBenches(data))
+}
+
+export const fetchBench = (benchId) => async (dispatch) =>{
+  const res = await csrfFetch(`/api/benches/${benchId}`)
+
+  const data = await res.json();
+
+  dispatch(addBench(data))
+}
+
+export const createBench = (benchData) => async (dispatch) =>{
+  const res = await csrfFetch(`/api/benches}`,{
+    method: "POST",
+    body: JSON.stringify(benchData),
+    headers: {"Content-Type":"application/json"}
+  })
+
+  const data = await res.json();
+
+  dispatch(addBench(data))
+}
+
+export default function benchesReducer(state={}, action){
+  const nextState = { ...state }
+  switch (action.type){
+    case SET_BENCHES:
+      return { ...state, ...action.benches }
+    case ADD_BENCH:
+      nextState[action.bench.id]=action.bench
+      return nextState
+    default: return state
+  }
+}
